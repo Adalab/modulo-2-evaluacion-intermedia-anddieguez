d68ca11dcd178ba101564b6d38bba1cbee3d0f85
anddieguez <andreadiegar@gmail.com>
2023-09-19 10:07:37 +0200

    added HTML and const

---
+++ main.js
@@ -0,0 +1,24 @@
+
+// Crear la estructura del HTML
+// poner clases de JS a los elementos de HTML
+// sacar los elementos de HTML que vayamos a utilizar
+//Escuchar el evento con su funcion manejadora
+//Poner event.PreventDefault() solo si es necesario.
+//crear la función para recoger los valores
+//dentro de handleClick hacer toda la lógica de lo que estan pidiendo
+
+const select = document.querySelector('.js-select');
+const msj = document.querySelector('.js-msj');
+const pJugador = document.querySelector('.js-p-jugador');
+const pComputadora = document.querySelector('.js-p-computadora');
+const btnPlay = document.querySelector('.js-btn-play');  
+
+const handleClick =()=>{
+
+};
+
+
+
+console.log('hola');
+
+btnPlay.addEventListener ("click", handleClick);
